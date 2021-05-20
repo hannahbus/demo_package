54f90be0cae886ada4686fa7baac009a9ab5e92b
Hannah Busshoff <hannah@eduroamverw-130-82-26-3.unisg.ch>
2021-05-20 09:37:52 +0200

    fct. updates to check multiprocessing

--- hbussi/beautify.py
+++ hbussi/beautify.py
@@ -1,20 +1,11 @@
 import concurrent.futures
-from concurrent.futures import freeze_support
+from multiprocessing import freeze_support
 import math
+import os
 
 def is_prime(n):
-    if n < 2:
-        return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-
-    sqrt_n = int(math.floor(math.sqrt(n)))
-    for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
-            return False
-    return True
+    process_id = n + os.getpid()
+    return process_id
 
 def main(PRIMES, addition = "test"):
     with concurrent.futures.ProcessPoolExecutor() as executor:
