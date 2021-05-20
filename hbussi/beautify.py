3241a4396725477cd9b59332048179b8bf7d18f5
Hannah Busshoff <hannah@eduroamverw-130-82-26-3.unisg.ch>
2021-05-20 09:59:54 +0200

    minor updates for new release

--- hbussi/beautify.py
+++ hbussi/beautify.py
@@ -7,12 +7,12 @@ def is_prime(n):
     process_id = os.getpid()
     return process_id
 
-def main(PRIMES, addition = "test"):
-    with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+def main(PRIMES):
+    with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
         for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
             print('%d is prime: %s' % (number, prime))
-            print(addition)
+           
 
 if __name__ == '__main__':
     freeze_support()
-    main(PRIMES, addition)
+    main(PRIMES)
