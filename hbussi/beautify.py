0e2f785237f6dcfc0a132e794dd89709a5e3c2fe
Hannah Busshoff <hannah@eduroamverw-130-82-26-3.unisg.ch>
2021-05-20 10:21:20 +0200

    heavier testing

--- hbussi/beautify.py
+++ hbussi/beautify.py
@@ -1,18 +1,31 @@
 import concurrent.futures
 from multiprocessing import freeze_support
+from numpy.linalg import inv
 import math
 import os
+import numpy as np
 
 def is_prime(n):
     process_id = os.getpid()
     return process_id
 
-def main(PRIMES):
-    with concurrent.futures.ProcessPoolExecutor(max_workers=6) as executor:
+def main(PRIMES, workers):
+    with concurrent.futures.ProcessPoolExecutor(max_workers= workers) as executor:
         for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
             print('%d is prime: %s' % (number, prime))
-           
+               
+def alternative(dim): 
+    m = np.random.rand(dim, dim)
+    m2 = m@m 
+    m = inv(m)
+    return True
+
+def main_2(dim, workers): 
+    with concurrent.futures.ProcessPoolExecutor(max_workers= workers) as executor:
+           for number, prime in zip(dim, executor.map(alternative, dim)):
+            print((number, prime))
+            
 
 if __name__ == '__main__':
     freeze_support()
-    main(PRIMES)
+    main_2(dim, workers)
