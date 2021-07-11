31fbbf433a4511191850bd0221ec279a775e0cf0
hannahbus <38669089+hannahbus@users.noreply.github.com>
2021-07-11 16:36:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   url = 'https://github.com/hannahbus/hbussi',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/hannahbus/hbussi/archive/refs/tags/v9.0.tar.gz',    # I explain this later on
   keywords = ['TEST'],   # Keywords that define your package best
-  install_requires=[],
+  install_requires=['numpy<=1.21.0'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
